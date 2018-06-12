026b13b0e0411ca97f3188023b7fd93bf80430ce
Josh Butler <joshua.butler@my.scitexas.edu>
2018-06-12 03:30:25 -0400

    Calculate rows

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const handleBars = require('express-handlebars').create({defaultLayout: 'main'});
+const handleBars = require('express-handlebars').create({defaultLayout: 'main', extname: '.handlebars'});
 const sqlite3 = require('sqlite3').verbose();
+const extend = require('handlebars-extend-block');
+var db = null;
+
+
+//Extend handleBars functionality
+handleBars.handlebars = extend(handleBars.handlebars);
 
 //Database Connection
 // open database in memory
-const db = new sqlite3.Database('./testdb.sqlite3', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to database.');
-});
+// const db = new sqlite3.Database('./testdb.sqlite3', (err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Connected to database.');
+// });
 // close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
-
-
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+// });
 
 const app = express();
+app.use((req, res, next) => {
+  db = new sqlite3.Database('./products.sqlite3', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to database.');
+  }); 
+    next();
+})
 
 //Configuration Setup
 app.use(express.static(__dirname + '/public'));
@@ -45,31 +58,29 @@ app.get('/beer-wine', (req, res) => {
 app.get('/create', (req, res) => {
   res.render('create')
 }) 
+
 app.get('/liquor', (req, res) => {
-  res.render('liquor')
+  db.all('SELECT * FROM liquor_table', [], (err, rows) => {
+    console.log(rows);
+    res.render('liquor', {liquors: rows})
+  })
+    db.close()
 })
+
 app.get('/misc', (req, res) => {
   res.render('misc')
 })
 
+
 //POST request
-app.post('/', (req, res, next) => {
-    console.log(results);
+app.post('/liquor', (req, res, next) => {
+  res.send('liquor')
 })
-
-
-
-
-
-
-
-
-
-
-
-
 
 //Listen On...
 app.listen(8080, () => {
   console.log('Server is Running!')
 })
+
+
+
